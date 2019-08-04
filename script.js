b9dd29cbdc2501db23a10e8b0440acc5b12f0c1a
Toms <donis.toms@gmail.com>
2019-08-04 21:10:11 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -15,15 +15,25 @@ function addToDo(){
   itemContainer.className = classNames.TODO_ITEM;
   // text
   var text = document.createTextNode(prompt());
-
-  // Append 
+ 
+  // append and create checkbox
+  itemContainer.appendChild(createNewCheckboxt('theName', 'theID')); //Create and append checkbox
   itemContainer.appendChild(text);
   list.appendChild(itemContainer);
-
   // Increment item counter
   counter++;
   itemCountSpan.innerHTML = counter;
 }
+
+// Create Checkbox
+function createNewCheckboxt(name, id){
+  var checkbox = document.createElement('input'); 
+  checkbox.type= 'checkbox';
+  checkbox.name = name;
+  checkbox.id = id;
+  return checkbox;
+}
+
 
 function newTodo() {
   addToDo();
